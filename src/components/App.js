0d3a3800751c31e1855b0c4e5520e422f539f51b
BertinoGH <bertino_gh10@hotmail.com>
2023-03-22 14:16:53 -0600

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { useState,useEffect } from "react";
 import Person from './Person';
-import axios from 'axios';
+import personService from '../services/persons';
+import Notification from "./Notifications";
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage,setErrorMessage]=useState(null)
   const handleNoteChange = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
@@ -18,40 +20,75 @@ const App = () => {
   }
 
   const addPersons = (event) => {
-    event.preventDefault()
-    if (persons.some(person => person.name.includes(newName))) {
-      alert(`${newName} is already added to phonebook`)
-    }
-    else {
+    event.preventDefault();
+    const existingPerson = persons.find((person) => person.name === newName);
+    if (existingPerson) {
+      const result = window.confirm(
+        `${newName} ya está en la lista de contactos. ¿Quieres actualizar su número?`
+      );
+      if (result) {
+        const updatedPerson = { ...existingPerson, number: newNumber };
+        personService
+          .update(existingPerson.id, updatedPerson)
+          .then((response) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== existingPerson.id ? person : response
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+            setErrorMessage(`Updated ${newName} `);
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 5000);
+          });
+      }
+    } else {
       const personObject = {
         name: newName,
         number: newNumber,
-      }
-
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
-
-
+      };
+      personService.create(personObject).then((response) => {
+        setPersons(persons.concat(response));
+        setNewName("");
+        setNewNumber("");
+        setErrorMessage(`Added ${newName}`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
     }
+  };
 
-    
-
-    //console.log('BUtton clicked',event)
-  }
   useEffect(() => {
-    console.log('Entro al effect')
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        console.log('Entro al then')
-        setPersons(response.data)
-      })
+    personService
+    .getAll()
+    .then(response=>{setPersons(response)})
   })
+
+  const deleteContact = (id) => {
+    const result = window.confirm("¿Estás seguro de que quieres eliminar este contacto?");
+    if (result) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id !== id));
+        })
+        .catch((error) => {
+          setErrorMessage(`the contact ${persons.name} was already existed`);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+          setPersons(persons.filter((x) => x.id !== id));
+        });
+    }
+  };
 
   return (
     <div>
       <h2>PhoneBook</h2>
+      <Notification message={errorMessage}/>
       
       <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} placeholder="Search..."></input>
 
@@ -72,7 +109,7 @@ const App = () => {
 
       <ul>
         {persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase())).map(person => (
-          <Person key={person.name} person={person} />
+          <Person key={person.name} person={person} deleteContact={()=>deleteContact(person.id)}/>
         ))}
       </ul>
 
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const Person=({person}) => <li>{person.name}, {person.number}</li>
+const Person = ({ person, deleteContact }) => {
+    const label = "delete"
+    return (
+        <li className="note">
+            {person.name}, {person.number}
+            <button onClick={deleteContact}>{label}</button>
+        </li>
+    )
+}
 
 export default Person
